70a9e975b1067dc4b8099ff4273afe1bdb7b30aa
kevhnmay94 <kevhnmay94@gmail.com>
2019-12-19 18:05:13 +0700

    image classification

--- imageclassification.py
+++ imageclassification.py
@@ -2,11 +2,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os, sys
 from pathlib import Path
+import time
 
 import tensorflow as tf
-
+from tensorflow.keras.applications import Xception, xception, MobileNetV2, mobilenet_v2, ResNet50, resnet50
 from tensorflow.keras import datasets, models
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
@@ -17,17 +19,17 @@ fitModel = False
 plotOn = False
 FILE_DIR = str(Path(sys.argv[0]).parent) + str(os.sep)
 
+image_path = "cats_and_dogs_filtered/square.jpg"
+
 
 def cifar():
     global fitModel, plotOn
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
+    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                   'dog', 'frog', 'horse', 'ship', 'truck']
     # Normalize pixel values to be between 0 and 1
     train_images, test_images = train_images / 255.0, test_images / 255.0
     if plotOn:
-        class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-                       'dog', 'frog', 'horse', 'ship', 'truck']
-
         plt.figure(figsize=(10, 10))
         for i in range(25):
             plt.subplot(5, 5, i + 1)
@@ -41,7 +43,7 @@ def cifar():
         plt.show()
 
     try:
-        model = models.load_model(FILE_DIR+'cifar.pkl')
+        model = models.load_model(FILE_DIR + 'cifar.pkl')
     except IOError as err:
         print(err)
         model = None
@@ -65,8 +67,37 @@ def cifar():
                       loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
     if fitModel:
-        history = model.fit(train_images, train_labels, epochs=5,
-                            validation_data=(test_images, test_labels))
+        datagen = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            zca_epsilon=1e-06,  # epsilon for ZCA whitening
+            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+            # randomly shift images horizontally (fraction of total width)
+            width_shift_range=0.1,
+            # randomly shift images vertically (fraction of total height)
+            height_shift_range=0.1,
+            shear_range=0.,  # set range for random shear
+            zoom_range=0.,  # set range for random zoom
+            channel_shift_range=0.,  # set range for random channel shifts
+            # set mode for filling points outside the input boundaries
+            fill_mode='nearest',
+            cval=0.,  # value used for fill_mode = "constant"
+            horizontal_flip=True,  # randomly flip images
+            vertical_flip=False,  # randomly flip images
+            # set rescaling factor (applied before any other transformation)
+            rescale=None,
+            # set function that will be applied on each input
+            preprocessing_function=None,
+            # image data format, either "channels_first" or "channels_last"
+            data_format=None,
+            # fraction of images reserved for validation (strictly between 0 and 1)
+            validation_split=0.0)
+        datagen.fit(train_images)
+        history = model.fit_generator(datagen.flow(train_images, train_labels), epochs=5,
+                                      validation_data=(test_images, test_labels))
         if plotOn:
             plt.plot(history.history['acc'], label='accuracy')
             plt.plot(history.history['val_acc'], label='val_accuracy')
@@ -75,10 +106,23 @@ def cifar():
             plt.ylim([0.5, 1])
             plt.legend(loc='lower right')
             plt.show()
-        model.save(FILE_DIR+'cifar.pkl')
+        model.save(FILE_DIR + 'cifar.pkl')
 
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
     print(test_acc)
+
+    img = load_img(image_path, target_size=(32, 32))
+    plt.imshow(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+    result = model.predict_proba(img)
+    arr = np.array(result[0])
+    print(result)
+    top_three = arr.argsort()[::-1][:3]
+    label = class_names[np.argmax(result[0])]
+    print(label)
+    plt.title(label)
+    plt.show()
 
 
 def plotImages(images_arr):
@@ -93,7 +137,7 @@ def plotImages(images_arr):
 
 def catdog():
     global fitModel, plotOn
-    PATH = os.path.join(FILE_DIR,'cats_and_dogs_filtered')
+    PATH = os.path.join(FILE_DIR, 'cats_and_dogs_filtered')
     print(PATH)
     train_dir = os.path.join(PATH, 'train')
     validation_dir = os.path.join(PATH, 'validation')
@@ -180,7 +224,6 @@ def catdog():
             plt.show()
         model.save('catdog.pkl')
 
-    image_path = "cats_and_dogs_filtered/odie.jpg"
     img = load_img(image_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
     plt.imshow(img)
     img = np.expand_dims(img, axis=0)
@@ -195,5 +238,41 @@ def catdog():
     plt.show()
 
 
+def imagenet(model_name):
+    start_time = time.process_time()
+    if model_name == 'xception':
+        model = Xception(weights='imagenet')
+        preprocess_input = xception.preprocess_input
+        decode_predictions = xception.decode_predictions
+        size = (299, 299)
+    elif model_name == 'mobilenet':
+        model = MobileNetV2(weights='imagenet')
+        preprocess_input = mobilenet_v2.preprocess_input
+        decode_predictions = mobilenet_v2.decode_predictions
+        size = (224, 224)
+    elif model_name == 'resnet50':
+        model = ResNet50(weights='imagenet')
+        preprocess_input = resnet50.preprocess_input
+        decode_predictions = resnet50.decode_predictions
+        size = (224, 224)
+    else:
+        print("No model found")
+        return
+    img = load_img(image_path, target_size=size)
+    plt.imshow(img)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    pred = model.predict(x)
+    end_time = time.process_time()
+    elapsed = end_time - start_time
+    print("Process time: ", f'{elapsed:.3f}s')
+    print('Predictions:')
+    for a in decode_predictions(pred, top=5)[0]:
+        print(a[1], ": ", f'{a[2] * 100:.2f}%')
+
+
 if __name__ == "__main__":
-    catdog()
+    imagenet('xception')
+    imagenet('mobilenet')
+    imagenet('resnet50')
