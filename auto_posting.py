534a6a10d8069e8bd1682174492e9a8054d9ca01
rifqyfr <fortune.cosine@gmail.com>
2020-06-13 18:04:57 +0700

    Commit perbaikan hide path

--- auto_posting.py
+++ auto_posting.py
@@ -39,8 +39,7 @@ def retrieve_post_tuple(url: str, post_list: list, unique_id: int, f_pin: str, p
         curtime_milli = int(round(time.time() * 1000))
         img_filename = ""
 
-        base_path_img = "/apps/devib/server/image"
-        # base_path_img = "/apps/indonesiabisa/server/image"
+        base_path_img = props[4]
         image_total = ""
         if image_src:
             n = 0
